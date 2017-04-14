1aca0e86eadee70105ad5f3660a92622bb2d816d
spgitmonish <monishsp@gmail.com>
2017-04-14 14:07:19 -0600

    Modify/Add comments

--- CarND-Extended-Kalman-Filter-Project/src/main.cpp
+++ CarND-Extended-Kalman-Filter-Project/src/main.cpp
@@ -66,112 +66,163 @@ int main(int argc, char* argv[]) {
 
   string line;
 
-  // prep the measurement packages (each line represents a measurement at a
-  // timestamp)
-  while (getline(in_file_, line)) {
-
+  // Prep the measurement packages (each line represents a measurement at a
+  // particular timestamp)
+  while (getline(in_file_, line))
+  {
+    // String object for storing the sensor type
     string sensor_type;
+    // Measurement package object
     MeasurementPackage meas_package;
+    // Ground truth package(actual values)
     GroundTruthPackage gt_package;
+    // Object which breaks down the passed in string into words
     istringstream iss(line);
+    // Variable for storing the time stamp
     long long timestamp;
 
-    // reads first element from the current line
+    // Get the first word in the line(or character)
     iss >> sensor_type;
-    if (sensor_type.compare("L") == 0) {
-      // LASER MEASUREMENT
 
-      // read measurements at this timestamp
+    // LASER/LIDAR MEASUREMENT
+    if (sensor_type.compare("L") == 0)
+    {
+      // Store the measurement source
       meas_package.sensor_type_ = MeasurementPackage::LASER;
+
+      // Vector for storing the position
       meas_package.raw_measurements_ = VectorXd(2);
+
+      // Variables to store x and y values(px, py)
       float x;
       float y;
+
+      // Get the next two words which are measurements
       iss >> x;
       iss >> y;
-      meas_package.raw_measurements_ << x, y;
-      iss >> timestamp;
-      meas_package.timestamp_ = timestamp;
-      measurement_pack_list.push_back(meas_package);
-    } else if (sensor_type.compare("R") == 0) {
-      // RADAR MEASUREMENT
 
-      // read measurements at this timestamp
+      // Store the position values
+      meas_package.raw_measurements_ << x, y;
+
+      // Get the time stamp
+      iss >> timestamp;
+
+      // Store the timestamp
+      meas_package.timestamp_ = timestamp;
+
+      // Push this measurement into the vector
+      measurement_pack_list.push_back(meas_package);
+    }
+    // RADAR MEASUREMENT
+    else if (sensor_type.compare("R") == 0)
+    {
+      // Store the measurement source
       meas_package.sensor_type_ = MeasurementPackage::RADAR;
+
+      // Vector for storing the position
       meas_package.raw_measurements_ = VectorXd(3);
+
+      // Variables to store the rho(range), phi(angle) and rho_dot(magnitude)
       float ro;
       float phi;
       float ro_dot;
+
+      // Get the respective values
       iss >> ro;
       iss >> phi;
       iss >> ro_dot;
+
+      // Store the measurements in the vector
       meas_package.raw_measurements_ << ro, phi, ro_dot;
+
+      // Get the timestamp
       iss >> timestamp;
+
+      // Store the timestamp
       meas_package.timestamp_ = timestamp;
+
+      // Push the values into the vector
       measurement_pack_list.push_back(meas_package);
     }
 
-    // read ground truth data to compare later
+    // Read ground truth data to compare later(error/accuracy calculation)
     float x_gt;
     float y_gt;
     float vx_gt;
     float vy_gt;
+
+    // Get the ground truth values
     iss >> x_gt;
     iss >> y_gt;
     iss >> vx_gt;
     iss >> vy_gt;
+
+    // An empty vector
     gt_package.gt_values_ = VectorXd(4);
+
+    // Push the ground truth
     gt_package.gt_values_ << x_gt, y_gt, vx_gt, vy_gt;
+
+    // Add the values to the vector
     gt_pack_list.push_back(gt_package);
   }
 
   // Create a Fusion EKF instance
   FusionEKF fusionEKF;
 
-  // used to compute the RMSE later
+  // Used to compute the RMSE later
   vector<VectorXd> estimations;
   vector<VectorXd> ground_truth;
 
-  //Call the EKF-based fusion
+  // Call the EKF-based fusion
   size_t N = measurement_pack_list.size();
-  for (size_t k = 0; k < N; ++k) {
-    // start filtering from the second frame (the speed is unknown in the first
-    // frame)
+
+  // For each of the measurements in the vector
+  for (size_t k = 0; k < N; ++k)
+  {
+    // Start filtering from the second frame, so the initial state needs to be
+    // set in the EKF object routines as the speed is unknown in the first frame
     fusionEKF.ProcessMeasurement(measurement_pack_list[k]);
 
-    // output the estimation
+    // Output the estimation
     out_file_ << fusionEKF.ekf_.x_(0) << "\t";
     out_file_ << fusionEKF.ekf_.x_(1) << "\t";
     out_file_ << fusionEKF.ekf_.x_(2) << "\t";
     out_file_ << fusionEKF.ekf_.x_(3) << "\t";
 
-    // output the measurements
-    if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::LASER) {
-      // output the estimation
+    // Output the measurements
+    if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::LASER)
+    {
+      // Output the estimation
       out_file_ << measurement_pack_list[k].raw_measurements_(0) << "\t";
       out_file_ << measurement_pack_list[k].raw_measurements_(1) << "\t";
-    } else if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::RADAR) {
-      // output the estimation in the cartesian coordinates
+    }
+    else if (measurement_pack_list[k].sensor_type_ == MeasurementPackage::RADAR)
+    {
+      // Output the estimation in the cartesian coordinates
       float ro = measurement_pack_list[k].raw_measurements_(0);
       float phi = measurement_pack_list[k].raw_measurements_(1);
+
       out_file_ << ro * cos(phi) << "\t"; // p1_meas
       out_file_ << ro * sin(phi) << "\t"; // ps_meas
     }
 
-    // output the ground truth packages
+    // Output the ground truth vaues
     out_file_ << gt_pack_list[k].gt_values_(0) << "\t";
     out_file_ << gt_pack_list[k].gt_values_(1) << "\t";
     out_file_ << gt_pack_list[k].gt_values_(2) << "\t";
     out_file_ << gt_pack_list[k].gt_values_(3) << "\n";
 
+    // Push the estimations and the ground truth
     estimations.push_back(fusionEKF.ekf_.x_);
     ground_truth.push_back(gt_pack_list[k].gt_values_);
   }
 
-  // compute the accuracy (RMSE)
+  // Compute the accuracy (RMSE)
   Tools tools;
   cout << "Accuracy - RMSE:" << endl << tools.CalculateRMSE(estimations, ground_truth) << endl;
 
-  // close files
+  // Close all the files
   if (out_file_.is_open()) {
     out_file_.close();
   }
